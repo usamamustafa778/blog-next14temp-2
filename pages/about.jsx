c574fe67b5c912dae752fd55c0a03f80ce4ef8b1
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2024-04-24 18:47:14 +0500

    about page text

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,11 +9,16 @@ import { Cormorant } from "next/font/google";
 import { cn } from "@/lib/utils";
 import Rightbar from "@/components/containers/Rightbar";
 import Head from "next/head";
+import MarkdownIt from "markdown-it";
 
 const myFont = Montserrat({ subsets: ["cyrillic"] });
 const font2 = Cormorant({ subsets: ["cyrillic"] });
 
-export default function About({ logo }) {
+export default function About({ logo, about_me }) {
+  const markdownIt = new MarkdownIt();
+  const content = markdownIt.render(about_me);
+
+  console.log(about_me);
   return (
     <div className={myFont.className}>
       <Head>
@@ -33,26 +38,10 @@ export default function About({ logo }) {
               >
                 LIFESTYLE BLOGGER
               </p>
-              <h2 className="text-7xl mt-6">Hi, I am Carolyn!</h2>
-              <p className="mt-5 text-lg">
-                Ex azum san pericula usu an, in pri dolorum denique. Cu movet
-                debitis cum. Vix paulo evertitur cu, conceptam constituam te
-                his, meis euismod sed ad. Partem suscipit cu cum. Vix no
-                prodesset complectitur conclud aturque, dicam perfecto eu per,
-                ne vis eleifend expetenda. Idque petentium laboramus mea id, mei
-                at.
-              </p>
-              <p className="mt-5">
-                Ex mea oratio evertitur, mediocrem principes no has. Quodsi
-                copiosae vel eu. Te rebum diceret gloriatur sea, dolorum
-                fabellas vulputate has ex, mea similique posidonium persequeris
-                at. Et dicat referrentur usu. Te cum saperet inimicus. Vix paulo
-                evertitur cu, conceptam constituam te his, meis euismod sed ad.
-                Partem suscipit cu cum. Vix no prodesset complectitur conclud
-                aturque, dicam perfecto eu per, ne vis eleifend expetenda. Idque
-                petentium laboramus mea id, mei at minim labores tollit enim
-                singulis instructior his dicant fabulas.
-              </p>
+              <div
+                className="markdown-content about_me"
+                dangerouslySetInnerHTML={{ __html: content }}
+              />
             </div>
             <Rightbar />
           </div>
@@ -73,9 +62,19 @@ export async function getStaticProps() {
   );
   const logo = await _logo.json();
 
+  const _about_me = await fetch(
+    `${
+      process.env.NEXT_PUBLIC_SITE_MANAGER
+    }/api/public/industry_template_data/${
+      process.env.NEXT_PUBLIC_INDUSTRY_ID
+    }/${process.env.NEXT_PUBLIC_TEMPLATE_ID}/data/${"about_me"}`
+  );
+  const about_me = await _about_me.json();
+
   return {
     props: {
       logo: logo.data[0],
+      about_me: about_me.data[0].value,
     },
   };
 }
